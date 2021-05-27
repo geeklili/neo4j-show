d0b093088b7ddc2ee8c3011c41a304fce40bb9da
geeklili <netgeek@163.com>
2021-05-27 16:36:31 +0800

    port

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ app.register_blueprint(bp, url_prefix='/')
 
 if __name__ == '__main__':
     # 生产状态可以把debug调成False
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
