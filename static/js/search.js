49250b2404953cc988e9643c024527afb23b67bb
geeklili <netgeek@163.com>
2021-05-28 14:17:26 +0800

    port

--- static/js/search.js
+++ static/js/search.js
@@ -1,71 +1,90 @@
-        $(function(){
-                $('#input').bind('keypress',function(event){
-                　if(event.keyCode==13){
-                    var keyword = $("#input").val();
-                    window.location.href='/api/kn?entity_name='+keyword
-                　return false;
-                　}
-                });
+$(function(){
+            $('#input').bind('keypress',function(event){
+            　if(event.keyCode==13){
+                var keyword = $("#input").val();
+                window.location.href='/api/kn?entity_name='+keyword
+            　return false;
+            　}
+            });
 
-                $('input').on({
-                        keyup : function(e){
-                                var obj =  document.getElementById("line_id")
-                                if (obj){
-                                        document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
-                                }else{
-                                        document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
+            function search_entity_and_add_html (data, entity) {
+                     document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
+                     var listShow = eval(data)
+                     // var listShow = ['a','b','c'];
+                     for (var index = 0; index < listShow.length; index++) {
+                         var backword = listShow[index];
 
-                                };
-                        },
-                        input : function(e){
-                            //console.log($(this).val()+'keyup')
-                            $(".show").find("div").remove();
-                            var entity = $(this).val();
-
-                             if (entity){
-                                  $.ajax({
-                                         url: "/api/around_data",
-                                         type: "GET",
-                                         jsonpCallback: "entity_search",
-                                         data: {'word': entity},
-                                         async: true,
-                                         success: function (data) {
-                                             $(".show").find("div").remove();
-                                             console.log('success entity: '+entity);
-                                             entity_search(data, entity)
-                                         },
-                                         error:function(data){
-                                             console.log("error")
-                                         }
-                                     });
-                             }
-
-
-                             entity_search = function (data, entity) {
-                                     document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
-                                     var listShow = eval(data)
-                                     // var listShow = ['a','b','c'];
-                                     for (var index = 0; index < listShow.length; index++) {
-                                         var backword = listShow[index];
-
-                                         if (listShow.length != index+1){
-                                             $everyLine = '<a href="/api/kn?entity_name=' + backword + '" style="text-decoration:none;color:black"><div class="line" id="line_id">' + backword + '</div></a>';
-                                             $(".show").append($everyLine);
-                                         }else{
-                                             $everyLine = '<a href="/api/kn?entity_name=' + backword + '" style="text-decoration:none;color:black"><div class="line_bottom" id="line_bottom_id">' + backword + '</div></a>';
-                                             $(".show").append($everyLine);
-                                             };
-                                     };
-
-                                     if (listShow.length > 0){
-                                         document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius:0px");
-                                     }else{
-                                         document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius:9px");
-                                     };
-
+                         if (listShow.length != index+1){
+                             $everyLine = '<a href="/api/kn?entity_name=' + backword + '" style="text-decoration:none;color:black"><div class="line" id="line_id">' + backword + '</div></a>';
+                             $(".show").append($everyLine);
+                         }else{
+                             $everyLine = '<a href="/api/kn?entity_name=' + backword + '" style="text-decoration:none;color:black"><div class="line_bottom" id="line_bottom_id">' + backword + '</div></a>';
+                             $(".show").append($everyLine);
                              };
+                     };
 
-                        },
-                })
+                     if (listShow.length > 0){
+                         document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius:0px");
+                     }else{
+                         document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius:9px");
+                     };
 
-    })
+            };
+
+            function ajax_request(){
+                    $(".show").find("div").remove();
+                    var entity = $("#input").val();
+
+                     if (entity){
+                          $.ajax({
+                                 url: "/api/around_data",
+                                 type: "GET",
+                                 jsonpCallback: "entity_search",
+                                 data: {'word': entity},
+                                 async: true,
+                                 success: function (data) {
+                                     $(".show").find("div").remove();
+                                     console.log('success entity: '+entity);
+                                     search_entity_and_add_html(data, entity)
+                                 },
+                                 error:function(data){
+                                     console.log("error")
+                                 }
+                             });
+                     }
+
+            };
+
+
+            function not_data_radius_not_change(){
+                    var obj =  document.getElementById("line_id") || document.getElementById("line_bottom_id")
+                    if (obj){
+                            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
+                    }else{
+                            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
+
+                    };
+
+            };
+
+
+            $('#input').on({
+                    focus : function(e){
+                        ajax_request();
+                    },
+
+                    keyup : function(e){
+                            not_data_radius_not_change();
+
+                    },
+
+                    input : function(e){
+                        ajax_request();
+
+                    },
+            })
+
+
+
+
+})
--- static/js/select.js
+++ static/js/select.js
@@ -1,53 +1,75 @@
-    var a = 0;
+var a = 0;
+var b = 0;
+var hide = 1;
 
-    function search_entity(){
-        var keyword = $("#input").val();
-        window.location.href='/api/kn?entity_name='+keyword;
-    };
+function search_entity(){
+    var keyword = $("#input").val();
+    window.location.href='/api/kn?entity_name='+keyword;
+};
 
-    function select_entity(){
-        var keyword = $("#select").val();
-        window.location.href='/api/kn?entity_name='+keyword;
 
-    };
+function select_entity(){
+    var keyword = $("#select").val();
+    window.location.href='/api/kn?entity_name='+keyword;
 
-    function input_click(){
-        a = 1;
-        var obj =  document.getElementById("line_id")
-        if (obj){
-                document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
-        }else{
-                document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
+};
 
-        };
-    };
 
-    function loss_focus(){
-        var obj =  document.getElementById("line_id")
-        if(!obj){
-                console.log('没有line，失去焦点')
-                document.getElementById("bbox").setAttribute("style", "border:1px solid #C7C4CE");
-                a = 0;
-        };
+function input_click(){
+    a = 1;
+    var obj =  document.getElementById("line_id") || document.getElementById("line_bottom_id")
+    if (obj){
+            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
+    }else{
+            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
 
     };
+    document.getElementById('show_id').style.display="block";
+    hide = 0
+};
 
-    function get_mouse(){
-        var obj =  document.getElementById("line_id")
-        if (obj){
-                document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
-        }else{
-                document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
 
-        };
-        console.log('进去')
-    };
-
-    function loss_mouse(){
-        if (a==0){
+function loss_focus(){
+    console.log('没有line，失去焦点')
+    if (b==0){
             document.getElementById("bbox").setAttribute("style", "border:1px solid #C7C4CE");
-        }else{
-            console.log('else')
-        };
+            document.getElementById('show_id').style.display="none";
+            hide = 1;
+            };
+
+    a = 0;
+};
+
+
+
+function get_mouse(){
+    var obj =  document.getElementById("line_id") || document.getElementById("line_bottom_id")
+    if (hide==0){
+            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9;border-bottom-left-radius: 0px;");
+    }else{
+            document.getElementById("bbox").setAttribute("style", "border:1px solid #4E62D9");
 
     };
+    console.log('进去')
+};
+
+
+
+function loss_mouse(){
+    if (a==0){
+        document.getElementById("bbox").setAttribute("style", "border:1px solid #C7C4CE");
+    }else{
+        console.log('else')
+    };
+
+};
+
+function in_div(){
+console.log('in_div')
+    b = 1
+}
+
+function out_div(){
+    b = 0
+console.log('in_div')
+}
